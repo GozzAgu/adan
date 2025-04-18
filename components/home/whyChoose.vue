57b3b8344a8fcb432c3ab3497a23a2123d77e569
GozzAgu <gozieagu1@gmail.com>
2025-04-18 05:00:52 -0700

    ...

--- components/home/whyChoose.vue
+++ components/home/whyChoose.vue
@@ -4,18 +4,9 @@
       <h2 class="text-xl md:text-4xl font-extrabold text-gray-800 dark:text-gray-200 mb-10 border-l-4 border-red-500 pl-4">
         Why Choose Us
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-12 items-center">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
         <div class="choose-us-card">
-          <Icon name="material-symbols:lightbulb-circle-rounded" class="text-green-500 text-4xl" />
-          <h3 class="text-green-500 dark:text-green-100 text-lg md:text-2xl font-bold mt-4">Innovative Solutions</h3>
-          <p class="mt-4 text-gray-600 dark:text-gray-300 leading-relaxed text-xs md:text-sm">
-            We leverage the latest technology to craft creative and innovative solutions, ensuring our 
-            products meet the evolving needs of our clients while pushing boundaries.
-          </p>
-        </div>
-        
-        <div class="choose-us-card">
-          <Icon name="material-symbols:person-raised-hand" class="text-blue-500 text-4xl" />
+          <Icon name="material-symbols:person-raised-hand" class="text-green-500 text-4xl" />
           <h3 class="text-blue-500 dark:text-blue-100 text-lg md:text-2xl font-bold mt-4">Expert Team</h3>
           <p class="mt-4 text-gray-600 dark:text-gray-300 leading-relaxed text-xs md:text-sm">
             Our diverse team of experts brings years of experience across various industries, ensuring 
@@ -24,7 +15,7 @@
         </div>
         
         <div class="choose-us-card">
-          <Icon name="material-symbols:person-play-rounded" class="text-red-500 text-4xl" />
+          <Icon name="material-symbols:person-play-rounded" class="text-blue-500 text-4xl" />
           <h3 class="text-red-500 dark:text-red-50 text-lg md:text-2xl font-bold mt-4">Client-Centric Approach</h3>
           <p class="mt-4 text-gray-600 dark:text-gray-300 leading-relaxed text-xs md:text-sm">
             We put our clients first, ensuring that every project is tailored to meet their specific needs. 
@@ -60,9 +51,7 @@
   @apply text-sm leading-relaxed;
 }
 
-.choose-us-card:nth-child(1) .icon {
-  @apply bg-green-100;
-}
+
 .choose-us-card:nth-child(2) .icon {
   @apply bg-blue-100;
 }
