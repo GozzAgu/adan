54cd5d39b375dd48a0644ce4c812f23c56f20550
GozzAgu <gozieagu1@gmail.com>
2025-05-05 17:46:45 +0100

    ///

--- components/home/hero.vue
+++ components/home/hero.vue
@@ -38,7 +38,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 const images = [
-  "https://images.unsplash.com/photo-1518837695005-2083093ee35b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1657368567330-662b92e03dcc?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1532787799187-93655e51d472?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
