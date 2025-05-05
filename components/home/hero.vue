a8ccc95ef99e825cf5c27f17e8083ef54e2f5ac0
GozzAgu <gozieagu1@gmail.com>
2025-05-05 17:41:01 +0100

    ..

--- components/home/hero.vue
+++ components/home/hero.vue
@@ -38,8 +38,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 const images = [
-  "https://images.unsplash.com/photo-1533077162801-86490c593afb?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1660997598847-bdad1ad226c7?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1518837695005-2083093ee35b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1532787799187-93655e51d472?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
 const content = ref(null);
