711afa0061eb20e829b7fa14645d4b0787c133ab
GozzAgu <gozieagu1@gmail.com>
2025-05-05 17:53:53 +0100

    ....

--- components/home/whyChoose.vue
+++ components/home/whyChoose.vue
@@ -4,24 +4,25 @@
       <h2 class="text-xl md:text-4xl font-extrabold text-gray-800 dark:text-gray-200 mb-10 border-l-4 border-red-500 pl-4">
         Why Choose Us
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+      <div class="grid grid-cols-1 md:grid-cols-1 gap-12 items-center">
         <div class="choose-us-card">
           <Icon name="material-symbols:person-raised-hand" class="text-green-500 text-4xl" />
-          <h3 class="text-blue-500 dark:text-blue-100 text-lg md:text-2xl font-bold mt-4">Expert Team</h3>
+          <!-- <h3 class="text-blue-500 dark:text-blue-100 text-lg md:text-2xl font-bold mt-4">Expert Team</h3> -->
           <p class="mt-4 text-gray-600 dark:text-gray-300 leading-relaxed text-xs md:text-sm">
-            Our diverse team of experts brings years of experience across various industries, ensuring 
-            that we can solve complex problems efficiently and deliver the highest quality outcomes.
+            With years of experience in water testing, compliance, and environmental consulting, 
+            we offer reliable, expert solutions that help businesses, cities, and homeowners stay safe, 
+            up to code, and environmentally responsible
           </p>
         </div>
         
-        <div class="choose-us-card">
+        <!-- <div class="choose-us-card">
           <Icon name="material-symbols:person-play-rounded" class="text-blue-500 text-4xl" />
           <h3 class="text-red-500 dark:text-red-50 text-lg md:text-2xl font-bold mt-4">Client-Centric Approach</h3>
           <p class="mt-4 text-gray-600 dark:text-gray-300 leading-relaxed text-xs md:text-sm">
             We put our clients first, ensuring that every project is tailored to meet their specific needs. 
             Our commitment to exceptional customer service is at the heart of our success.
           </p>
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
